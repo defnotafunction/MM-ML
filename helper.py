c255997c04a0e9bb36972e6239c4ae8b0d25b862
Aiden Katambwa <aidenkatambwa7@gmail.com>
2026-03-27 20:55:20 -0500

    Use forest only, make predictions symmetrical

--- helper.py
+++ helper.py
@@ -24,7 +24,7 @@ m_teams.set_index('TeamID', inplace=True)
 # HELPER FUNCTIONS
 def get_team_from_id(team_id: int) -> str:
     """Gets team row from id"""
-    return m_teams.loc[team_id]
+    return m_teams.loc[team_id]['TeamName']
 
 def get_id_from_teamname(team_name: str) -> int:
     """Gets ID from name of team"""
@@ -69,66 +69,47 @@ def create_tourney_averages_frame(seasons=recent_seasons) -> pd.DataFrame:
 
     return pd.DataFrame(teams_averages).T
 
-def get_team_averages(team_name) -> pd.Series:
-    """Gets the average stats of a team"""
+REGULAR_AVG = create_regular_averages_frame()
+TOURNEY_AVG = create_tourney_averages_frame()
+
+def get_team_averages(team_name):
     team_id = get_id_from_teamname(team_name)
-    regular_avg = create_regular_averages_frame().loc[team_id]
-    tourney_avg = create_tourney_averages_frame().loc[team_id].fillna(regular_avg)
-    combined_avg = pd.concat([regular_avg, tourney_avg], axis=0)
-    return combined_avg
+
+    regular_avg = REGULAR_AVG.loc[team_id]
+    tourney_avg = TOURNEY_AVG.loc[team_id].fillna(regular_avg)
+
+    combined = pd.concat([
+        regular_avg.add_suffix('_reg'),
+        tourney_avg.add_suffix('_tourney')
+    ])
+
+    return combined
 
 def get_vectorized_data_from_teams(team1, team2):
     team1_avg, team2_avg = get_team_averages(team1), get_team_averages(team2)
     differences = [t1 - t2 for t1, t2 in zip(team1_avg.values, team2_avg.values)]
     return differences
 
-def get_vectorized_data(seed=1, _type: str ='regular', seasons: list = recent_seasons):
+
+def get_vectorized_data(seed=1):
     random.seed(seed)
-    if _type == 'regular':
-        regular_averages = create_regular_averages_frame(seasons)
-        team_stats_dict = regular_averages.to_dict(orient='index')
-        valid_team_ids = set(team_stats_dict.keys())
-    elif _type == 'tourney':
-        tourney_averages = create_tourney_averages_frame(seasons)
-        team_stats_dict = tourney_averages.to_dict(orient='index')
-        valid_team_ids = set(tourney_averages.index)
-    else:
-        combined = create_regular_averages_frame(seasons).join(
-                                            create_tourney_averages_frame(seasons),
-                                            how='inner',
-                                            lsuffix='_reg',
-                                            rsuffix='_tourney'
-                                        )
-        team_stats_dict = combined.to_dict(orient='index')
-        valid_team_ids = set(combined.index)
 
     features = []
     labels = []
 
-    all_winning_teams, all_losing_teams = regular_stats['WTeamID'], regular_stats['LTeamID']
-
-    for w1, l1 in zip(all_winning_teams, all_losing_teams):
-        if w1 not in valid_team_ids or l1 not in valid_team_ids:
+    for w, l in zip(regular_stats['WTeamID'].to_list(), regular_stats['LTeamID'].to_list()):
+        if w not in REGULAR_AVG.index or l not in REGULAR_AVG.index:
             continue
-
-        team1 = team_stats_dict[w1]
-        team2 = team_stats_dict[l1]
-
-        team1_vals = list(team1.values())
-        team2_vals = list(team2.values())
-
-        if any(np.isnan(team1_vals)) or any(np.isnan(team2_vals)):
-            continue
+        
+        w = get_team_from_id(w)
+        l = get_team_from_id(l)
 
         if random.random() > 0.5:
-            differences = [team1_val - team2_val for team1_val, team2_val in zip(team1_vals, team2_vals)]
-            label = 1  # 1 = win
+            features.append(get_vectorized_data_from_teams(w, l))
+            labels.append(1)
         else:
-            differences = [team2_val - team1_val for team1_val, team2_val in zip(team1_vals, team2_vals)]
-            label = 0
-
-        labels.append(label)
-        features.append(differences)
+            features.append(get_vectorized_data_from_teams(l, w))
+            labels.append(0)
 
     return np.array(features), np.array(labels)
 
--- main.py
+++ main.py
@@ -1,33 +1,19 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC
 from helper import *
-from sklearn.preprocessing import StandardScaler
 import streamlit as st
 
 @st.cache_resource
-def train_models():
-    scaler = StandardScaler()
-    # Regular and Tourney datasets combined
-    combined_data, combined_labels = get_vectorized_data(_type='both')
-    combined_training, combined_testing, combined_training_labels, combined_test_labels = train_test_split(combined_data, combined_labels, test_size=0.3, random_state=1)
-    c_train_scaled = scaler.fit_transform(combined_training)
-    c_test_scaled = scaler.transform(combined_testing)
+def train_forest():
+    combined_data, combined_labels = get_vectorized_data()
 
     forest = RandomForestClassifier(random_state=1)
-    forest.fit(combined_training, combined_training_labels)
+    forest.fit(combined_data, combined_labels)
 
-    regression = LogisticRegression()
-    regression.fit(combined_training, combined_training_labels)
+    return forest
 
-    svc = SVC(C=.37)
-    svc.fit(c_train_scaled, combined_training_labels)
-    
-    return svc, regression, forest
-
-svc, regression, forest = train_models()
-
+forest = train_forest()
+print(forest)
 team_names = m_teams['TeamName'].tolist()
 
 st.title('March Madness Predictor')
@@ -38,5 +24,5 @@ if st.button("Predict Winner"):
     if not team1 or not team2:
         st.warning("Please enter both names")
     else:
-        winner = team1 if predict_winner(team1, team2, svc, regression, forest) == 1 else team2
+        winner = team1 if predict_winner(team1, team2, forest) == 1 else team2
         st.success(f'The predicted winner is: {winner}')
